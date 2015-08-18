5ee523fa22b3b0c21b39c360746e655dd83c105f
Andrea Zagli <andrea.zagli@email.it>
2015-08-18 20:07:52 +0200

    Initial import.

---
+++ src/gtkmapserver.c
@@ -0,0 +1,166 @@
+/*
+ *  gtkmapserver.c
+ *
+ *  Copyright (C) 2015 Andrea Zagli <azagli@libero.it>
+ *
+ *  This file is part of libgtkmapserver.
+ *
+ *  libgtk_mapserver is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  libgtk_mapserver is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with libgdaex; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#ifdef HAVE_CONFIG_H
+	#include <config.h>
+#endif
+
+#include <glib/gi18n-lib.h>
+#include <gtk/gtk.h>
+
+#ifdef G_OS_WIN32
+	#include <windows.h>
+#endif
+
+#include "gtkmapserver.h"
+
+static void gtk_mapserver_class_init (GtkMapserverClass *klass);
+static void gtk_mapserver_init (GtkMapserver *gtk_mapserver);
+
+static void gtk_mapserver_set_property (GObject *object,
+                               guint property_id,
+                               const GValue *value,
+                               GParamSpec *pspec);
+static void gtk_mapserver_get_property (GObject *object,
+                               guint property_id,
+                               GValue *value,
+                               GParamSpec *pspec);
+
+#define GTK_MAPSERVER_GET_PRIVATE(obj) (G_TYPE_INSTANCE_GET_PRIVATE ((obj), GTK_TYPE_MAPSERVER, GtkMapserverPrivate))
+
+typedef struct _GtkMapserverPrivate GtkMapserverPrivate;
+struct _GtkMapserverPrivate
+	{
+	};
+
+G_DEFINE_TYPE (GtkMapserver, gtk_mapserver, G_TYPE_OBJECT)
+
+#ifdef G_OS_WIN32
+static HMODULE hmodule;
+
+BOOL WINAPI
+DllMain (HINSTANCE hinstDLL,
+         DWORD     fdwReason,
+         LPVOID    lpvReserved)
+{
+	switch (fdwReason)
+		{
+			case DLL_PROCESS_ATTACH:
+				hmodule = hinstDLL;
+				break;
+		}
+
+	return TRUE;
+}
+#endif
+
+static void
+gtk_mapserver_class_init (GtkMapserverClass *klass)
+{
+	GObjectClass *object_class = G_OBJECT_CLASS (klass);
+
+	g_type_class_add_private (object_class, sizeof (GtkMapserverPrivate));
+
+	object_class->set_property = gtk_mapserver_set_property;
+	object_class->get_property = gtk_mapserver_get_property;
+}
+
+static void
+gtk_mapserver_init (GtkMapserver *gtk_mapserver)
+{
+	GtkMapserverPrivate *priv = GTK_MAPSERVER_GET_PRIVATE (gtk_mapserver);
+}
+
+GtkMapserver
+*gtk_mapserver_new ()
+{
+	gchar *localedir;
+
+	GtkMapserver *gtk_mapserver = GTK_MAPSERVER (g_object_new (gtk_mapserver_get_type (), NULL));
+
+	GtkMapserverPrivate *priv = GTK_MAPSERVER_GET_PRIVATE (gtk_mapserver);
+
+#ifdef G_OS_WIN32
+
+	gchar *moddir;
+	gchar *p;
+
+	moddir = g_win32_get_package_installation_directory_of_module (hmodule);
+
+	p = g_strrstr (moddir, g_strdup_printf ("%c", G_DIR_SEPARATOR));
+	if (p != NULL
+	    && (g_ascii_strcasecmp (p + 1, "src") == 0
+	        || g_ascii_strcasecmp (p + 1, ".libs") == 0))
+		{
+			localedir = g_strdup (LOCALEDIR);
+		}
+	else
+		{
+			localedir = g_build_filename (moddir, "share", "locale", NULL);
+		}
+
+	g_free (moddir);
+
+#else
+
+	localedir = g_strdup (LOCALEDIR);
+
+#endif
+
+	bindtextdomain (GETTEXT_PACKAGE, localedir);
+	textdomain (GETTEXT_PACKAGE);
+	bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
+
+	g_free (localedir);
+
+	return gtk_mapserver;
+}
+
+/* PRIVATE */
+static void
+gtk_mapserver_set_property (GObject *object, guint property_id, const GValue *value, GParamSpec *pspec)
+{
+	GtkMapserver *gtk_mapserver = GTK_MAPSERVER (object);
+	GtkMapserverPrivate *priv = GTK_MAPSERVER_GET_PRIVATE (gtk_mapserver);
+
+	switch (property_id)
+		{
+			default:
+				G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+				break;
+		}
+}
+
+static void
+gtk_mapserver_get_property (GObject *object, guint property_id, GValue *value, GParamSpec *pspec)
+{
+	GtkMapserver *gtk_mapserver = GTK_MAPSERVER (object);
+	GtkMapserverPrivate *priv = GTK_MAPSERVER_GET_PRIVATE (gtk_mapserver);
+
+	switch (property_id)
+		{
+			default:
+				G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+				break;
+		}
+}
---
+++ src/gtkmapserver.h
@@ -0,0 +1,64 @@
+/*
+ *  gtkmapserver.h
+ *
+ *  Copyright (C) 2015 Andrea Zagli <azagli@libero.it>
+ *
+ *  This file is part of libgtkmapserver.
+ *
+ *  libgdaex is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  libgdaex is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with libgdaex; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#ifndef __GTK_MAPSERVER_H__
+#define __GTK_MAPSERVER_H__
+
+#include <glib.h>
+#include <glib-object.h>
+#include <gtk/gtk.h>
+
+
+G_BEGIN_DECLS
+
+
+#define GTK_TYPE_MAPSERVER                 (gtk_mapserver_get_type ())
+#define GTK_MAPSERVER(obj)                 (G_TYPE_CHECK_INSTANCE_CAST ((obj), GTK_TYPE_MAPSERVER, GtkMapserver))
+#define GTK_MAPSERVER_CLASS(klass)         (G_TYPE_CHECK_CLASS_CAST ((klass), GTK_TYPE_MAPSERVER, GtkMapserverClass))
+#define GTK_IS_MAPSERVER(obj)              (G_TYPE_CHECK_INSTANCE_TYPE ((obj), GTK_TYPE_MAPSERVER))
+#define GTK_IS_MAPSERVER_CLASS(klass)      (G_TYPE_CHECK_CLASS_TYPE ((klass), GTK_TYPE_MAPSERVER))
+#define GTK_MAPSERVER_GET_CLASS(obj)       (G_TYPE_INSTANCE_GET_CLASS ((obj), GTK_TYPE_MAPSERVER, GtkMapserverClass))
+
+
+typedef struct _GtkMapserver GtkMapserver;
+typedef struct _GtkMapserverClass GtkMapserverClass;
+
+struct _GtkMapserver
+	{
+		GObject parent;
+	};
+
+struct _GtkMapserverClass
+	{
+		GObjectClass parent_class;
+	};
+
+GType gtk_mapserver_get_type (void) G_GNUC_CONST;
+
+
+GtkMapserver *gtk_mapserver_new (void);
+
+
+G_END_DECLS
+
+#endif /* __GTK_MAPSERVER_H__ */
